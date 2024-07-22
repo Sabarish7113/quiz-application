d5086267cac26329d6ec33eac73cc981145e85c4
Sabarish7113 <sabarish.user@gmail.com>
2024-07-22 19:34:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function val(choose){
-    const buttons=document.querySelectorAll("one");
-    buttons.forEach(button =>{
-        button.style.backgroundColor="white";
-    });
-    if(choosen.id=='correct'){
-        choosen.style.backgroundColor="green";
-        return true;
-    }
-    else{
-        choosen.style.backgroundColor="red";
-        return false;  
-    }
-    
+function checkAnswer(element, status) {
+    element.classList.remove('correct', 'wrong');
+    element.classList.add(status);
 }
